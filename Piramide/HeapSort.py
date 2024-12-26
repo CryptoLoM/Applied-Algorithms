90d9b9e0c4216657587997cc453bd915ab3515dd
Mat Johnson <7blickoff7@gmail.com>
2024-12-26 20:18:59 +0200

    first commit

--- Piramide/HeapSort.py
+++ Piramide/HeapSort.py
@@ -12,12 +12,14 @@ def heap_sort(arr):
 
     print("Sorting the elements:")
     for max_index in range(n - 1, 0, -1):
+        print(f"Remaining to the sorted array: {max_index - 1} iteration(s)")
         arr[0], arr[max_index] = arr[max_index], arr[0]
         print_heap(arr)
         heapify(arr, max_index, 0)
+    print("The array is sorted!")
 
 
-n = int(input("Enter number of elements:"))
+n = int(input("Enter number of elements: "))
 arr = [random.randint(1, 1000000) for _ in range(n)]
 
 print("Unsorted array:", arr)
